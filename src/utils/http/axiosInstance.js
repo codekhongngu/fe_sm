a3bacb10b6a1a3f772ef2faed332a28bf4ebcc44
Do Trong Thi <thidt.bdh@vnpt.vn>
2026-04-06 11:01:02 +0700

    update

--- src/utils/http/axiosInstance.js
+++ src/utils/http/axiosInstance.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || 'https://be-sm.codekhongngu.workers.dev',
-  withCredentials: true,
   timeout: 30000,
 });
 axiosInstance.interceptors.request.use((config) => {
