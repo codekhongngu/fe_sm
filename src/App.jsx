451d61f95fed7ea4c6a342ccceeb5dbfafbd95b9
Do Trong Thi <thidt.bdh@vnpt.vn>
2026-04-14 19:44:56 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
+import axios from 'axios';
 import Router from './routes/Router';
 import { restoreAuth } from './store/auth/AuthSlice';
-import axiosInstance from './utils/http/axiosInstance';
 import { BusinessTimeUtil } from './utils/BusinessTimeUtil';
 
 function App() {
@@ -11,8 +11,9 @@ function App() {
   useEffect(() => {
     dispatch(restoreAuth());
     
-    // Tải cấu hình giờ cắt ngày từ hệ thống
-    axiosInstance.get('/api/system-configs/cutoff-time')
+    // Tải cấu hình giờ cắt ngày từ hệ thống (Dùng axios gốc để không bị interceptor chặn lỗi 401)
+    const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://be-sm.codekhongngu.workers.dev';
+    axios.get(`${baseURL}/api/system-configs/cutoff-time`)
       .then(res => {
         if (res.data && res.data.hour !== undefined) {
           BusinessTimeUtil.CUTOFF_HOUR = Number(res.data.hour);
